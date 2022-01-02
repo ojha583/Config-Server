ac01ca2d62cd8b5167cead99ec162d25e5844ae9
aojha <aojha@jcp.com>
2022-01-02 13:36:01 +0530

    first commit for config server service

---
+++ src/test/java/com/domain/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domain.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
